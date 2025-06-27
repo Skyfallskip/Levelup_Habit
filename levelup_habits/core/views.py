e3fa217536bb73c4a04dc13467cb3c1cfb855e91
Mateus Lírio <mateuslirio3@gmail.com>
2025-06-27 12:16:12 +0000

    hot fix 7.1

--- levelup_habits/core/views.py
+++ levelup_habits/core/views.py
@@ -1,16 +1,13 @@
 from django.shortcuts import render
-from django.core.exceptions import PermissionDenied
 from habits.models import Habit
 
 def dashboard_view(request):
     if not request.user.is_authenticated:
-        raise PermissionDenied("Você não tem permissão para acessar esta página.")
+        return render(request, 'acess_denied.html', status=403)
     habits = Habit.objects.filter(user=request.user)
     context = {
         'habits': habits,
         'user': request.user,
-        'context' : Habit.description,
-        'title': Habit.title,
         'habit_count': habits.count(),
         'habit_completed_count': habits.filter(completed=True).count(),
         'habit_incomplete_count': habits.filter(completed=False).count(),
@@ -19,7 +16,7 @@ def dashboard_view(request):
             'incomplete': habits.filter(completed=False).count(),
         },
         'habit_streak': habits.filter(streak__gt=0).count(),
-        'level' : request.user.userprofile.level if hasattr(request.user, 'userprofile') else 1,
-        'xp' : request.user.userprofile.xp if hasattr(request.user, 'userprofile') else 0,
+        'level': getattr(getattr(request.user, 'userprofile', None), 'level', 1),
+        'xp': getattr(getattr(request.user, 'userprofile', None), 'xp', 0),
     }
     return render(request, 'home.html', context)
